360be781d0d2e4d46a84c349f953cf233dc571d6
Yang <wenpux.yang@intel.com>
2021-04-26 16:48:17 +0800

    xiugaisetting

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'e_d-*d^al%=cx5&#02y-a-og2tu@uhy@l*uwqmtc%y&@fhlblr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -71,10 +71,22 @@ WSGI_APPLICATION = 'django_auth_example.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_blog',        #数据库名字
+        'USER': 'root',          #账号
+        'PASSWORD': '1234567',      #密码
+        'HOST': '127.0.0.1',    #IP
+        'PORT': '3306',                   #端口
     }
 }
 
